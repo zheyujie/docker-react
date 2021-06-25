24666f752378d2a9cde9f8fd2d3427fd57939e56
Yujie Zhe <richardzhe@hotmail.com>
2021-06-25 15:43:06 +1200

    change message

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a feature branch
         </a>
       </header>
     </div>
